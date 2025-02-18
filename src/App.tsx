f642045781234532754563b33826fc9a2a3382bc
SIrGladkov <sir.gladkov@mail.ru>
2025-02-18 22:20:01 +0300

    docs: проверка работоспособности

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@
 
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
+import Home from '@/pages/Home';
+import About from '@/pages/About';
 
 const App: React.FC = () => {
   return (
